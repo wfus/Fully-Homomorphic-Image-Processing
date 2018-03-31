2df82f8ea6c11135786c52cab86bf2adcf7cd7df
GodPlex <keszler@college.harvard.edu>
2018-03-31 13:36:33 -0400

    Small refactor

---
+++ homo/sender.cpp
@@ -0,0 +1,16 @@
+#include "seal/seal.h"
+#include "fhe_image.h"
+
+using namespace seal;
+
+auto start = std::chrono::steady_clock::now();
+const bool VERBOSE = true;
+
+std::vector<double> read_image(std::string fname);
+
+int main()
+{
+
+
+    return 0;
+}
