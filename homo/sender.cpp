6473404c4ac3f0646784a468443479780d973785
GodPlex <keszler@college.harvard.edu>
2018-03-31 14:37:06 -0400

    heh

--- homo/sender.cpp
+++ homo/sender.cpp
@@ -1,6 +1,8 @@
 #include "seal/seal.h"
 #include "fhe_image.h"
 #include "jpge.h"
+#include "stb_image.c"
+
 
 using namespace seal;
 
@@ -11,7 +13,11 @@ std::vector<double> read_image(std::string fname);
 
 int main()
 {
-
-
+    const char* test_filename = "../image/kung.jpg";
+    const int requested_composition = 3;
+    int width = 0, height = 0, actual_composition = 0;
+    uint8_t *image_data = stbi_load(test_filename, &width, &height, &actual_composition, requested_composition);
+    std::cout << width << " x " << height << std::endl;
+    print_image(image_data,  width, height);
     return 0;
 }
