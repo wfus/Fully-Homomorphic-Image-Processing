28a714d22707973c2505c558ed1d41e82849a563
Raymond Lin <raylin1000@gmail.com>
2018-04-01 16:06:57 -0400

    Added new picture

--- homo/server.cpp
+++ homo/server.cpp
@@ -14,9 +14,6 @@ void fhe_jpg(std::vector<Ciphertext> &raw_data,
              FractionalEncoder &encoder,
              Encryptor &encryptor);
 
-const std::vector<double> S_STD_LUM_QUANT = { 16,11,12,14,12,10,16,14,13,14,18,17,16,19,24,40,26,24,22,22,24,49,35,37,29,40,58,51,61,60,57,51,56,55,64,72,92,78,64,68,87,69,55,56,80,109,81,87,95,98,103,104,103,62,77,113,121,112,100,120,92,101,103,99 };
-const std::vector<double> S_STD_CROMA_QUANT = { 17,18,18,24,21,24,47,26,26,47,99,66,56,66,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99 };
-
 int main(int argc, char** argv) {
 
     // Read encryption parameters from file
@@ -33,7 +30,7 @@ int main(int argc, char** argv) {
     EncryptionParameters params;
     params.set_poly_modulus("1x^8192 + 1");
     params.set_coeff_modulus(coeff_modulus_128(2048));
-    params.set_plain_modulus(1 << 14);
+    params.set_plain_modulus(1 << 12);
     SEALContext context(params);
     print_parameters(context);
 
