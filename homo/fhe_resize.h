f52fa3f7b5665a6f9808207d4a71fa4a78ba855d
WFU <wfu@college.harvard.edu>
2018-04-11 21:32:49 -0400

    Added new header file for resizing images

---
+++ homo/fhe_resize.h
@@ -0,0 +1,18 @@
+#ifndef FHE_RESIZE_H
+#define FHE_RESIZE_H
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <random>
+#include <cmath>
+
+#include "seal/seal.h"
+
+using namespace seal;
+
+
+
+#endif
