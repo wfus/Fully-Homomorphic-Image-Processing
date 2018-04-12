4a146a014f310590135dca98c32ce572147e39d1
WFU <wfu@college.harvard.edu>
2018-04-11 21:52:59 -0400

    Opencv include works, updated CMake and added test file

--- homo/fhe_resize.h
+++ homo/fhe_resize.h
@@ -10,9 +10,28 @@
 #include <cmath>
 
 #include "seal/seal.h"
+#include <opencv2/opencv.hpp>
 
 using namespace seal;
+using namespace cv;
 
 
+void show_image(const char* im_name) {
+
+    Mat image;
+    image = imread(im_name, 1);
+
+    if (!image.data) {
+        std::cout << "No image data..." << std::endl;
+        return;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    waitKey(0);
+}
+
+void show_image_rgb() {
+    return;
+}
 
 #endif
---
+++ homo/image_test.cpp
@@ -0,0 +1,9 @@
+#include "fhe_resize.h"
+
+int main(int argc, char** argv) {
+    show_image("image/coolboaz.jpg");
+    return 0;
+}
+
+
+
