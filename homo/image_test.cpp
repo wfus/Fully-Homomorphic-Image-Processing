3bc68bcc9f20d4afd291d32a45df1d30a0785e4f
WFU <wfu@college.harvard.edu>
2018-04-11 22:16:20 -0400

    w

--- homo/image_test.cpp
+++ homo/image_test.cpp
@@ -1,7 +1,11 @@
 #include "fhe_resize.h"
 
 int main(int argc, char** argv) {
-    show_image("image/coolboaz.jpg");
+    // show_image("image/coolboaz.jpg");
+    std::vector<uint8_t> red {12, 15, 222, 112, 115, 112,12, 15, 222, 112, 115, 112 ,12, 15, 222, 112, 115, 112};
+    std::vector<uint8_t> green {12, 15, 222, 112, 115, 112,12, 15, 222, 112, 115, 112 ,12, 15, 222, 112, 115, 112};
+    std::vector<uint8_t> blue {12, 15, 222, 112, 115, 112,12, 15, 222, 112, 115, 112 ,12, 15, 222, 112, 115, 112};
+    show_image_rgb(6, 3, red, green, blue);
     return 0;
 }
 
