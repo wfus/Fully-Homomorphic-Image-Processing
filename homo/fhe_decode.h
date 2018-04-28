fc5616307df5d196b0325a549693fa1213007bce
WFU <wfu@college.harvard.edu>
2018-04-28 15:45:07 -0400

    [decode] added comments to describe what we need

--- homo/fhe_decode.h
+++ homo/fhe_decode.h
@@ -13,4 +13,22 @@ double cosine[106];
 int coefficients[4096];
 
 
+/* We want to find the Fourier decomposition of a step function at b_1 and b_2, 
+ * and then 0 everywhere else from 0 to 64. 
+ * 1             ________
+ *              |        |
+ *              |        |
+ * 0  __________|        |_____________________
+ *   0         b1        b2                   64
+ * We will shift the origin to be at (b1 + b2)/2 and then calculate the Fourier 
+ * decomposition there for the coefficients. 
+ * 
+ * Returns a vector of coefficients and sine stuff
+ */
+void calculate_coefficients(int b1, int b2) {
+
+}
+
+
+
 #endif
