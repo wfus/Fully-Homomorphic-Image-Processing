da157fee5d65d51e1adc0412d79ed3ec7654a823
WFU <wfu@college.harvard.edu>
2018-04-29 16:00:27 -0400

    [decode] finished implementing and testing sine approximation

--- homo/fhe_decode.h
+++ homo/fhe_decode.h
@@ -49,6 +49,12 @@ void calculate_coefficients(std::vector<double> &coeff,
     }
 }
 
+void print_ciphertext_debug(Ciphertext &c, Decryptor &decryptor, FractionalEncoder &encoder) {
+    Plaintext p;
+    decryptor.decrypt(c, p);
+    std::cout << "Val: " << encoder.decode(p) << " Noise: " << decryptor.invariant_noise_budget(c) << std::endl;
+}
+
 /* We will approximate with the center at x = 3pi/2, because our values of b
  * will be between 1 to 64 at max, which means that with the coefficients
  * we will get better approximations because of range of B. The coefficients will be
@@ -56,28 +62,34 @@ void calculate_coefficients(std::vector<double> &coeff,
  * 1/720 (x - (3 \[Pi])/2)^6 - (x - (3 \[Pi])/2)^8/40320
  */
 Ciphertext homomorphic_sine(Ciphertext &x,
+                            Ciphertext &res,
                             Evaluator &evaluator, 
                             FractionalEncoder &encoder, 
-                            Encryptor &encryptor) {
-    Ciphertext res;
-    encryptor.encrypt(encoder.encode(0), res);
+                            Encryptor &encryptor
+                            ) {
+    
+    encryptor.encrypt(encoder.encode(0.0), res);
 
     Ciphertext shifted_x(x);
-    evaluator.sub_plain(shifted_x, encoder.encode(3 * M_PI));
+    evaluator.add_plain(shifted_x, encoder.encode(-3 * M_PI/2.0));
 
     Ciphertext power2(shifted_x);
     Ciphertext power4(shifted_x);
     Ciphertext power6(shifted_x);
     Ciphertext power8(shifted_x);
+    //Ciphertext power10(shifted_x);
+    //Ciphertext power12(shifted_x);
 
     // 2nd order term
     evaluator.square(power2);
     evaluator.multiply_plain(power2, encoder.encode(0.5));
+    // print_ciphertext_debug(power2, decryptor, encoder);
 
     // 4th order term
     evaluator.square(power4);
     evaluator.square(power4);
     evaluator.multiply_plain(power4, encoder.encode(-1.0/24.0));
+    // print_ciphertext_debug(power4, decryptor, encoder);
 
     // 6th order term 
     evaluator.square(power6);
@@ -85,12 +97,16 @@ Ciphertext homomorphic_sine(Ciphertext &x,
     evaluator.multiply(power6, shifted_x);     
     evaluator.multiply(power6, shifted_x);     
     evaluator.multiply_plain(power6, encoder.encode(1.0/720.0));
+    // print_ciphertext_debug(power6, decryptor, encoder);
 
     // 8th order term
     evaluator.square(power8);
     evaluator.square(power8);
     evaluator.square(power8);
     evaluator.multiply_plain(power8, encoder.encode(-1.0/40320.0));
+    // print_ciphertext_debug(power8, decryptor, encoder);
+    
+    // 10th order term
 
     // Add 0th order term and everything up 
     evaluator.add_plain(res, encoder.encode(-1.0));
@@ -98,8 +114,6 @@ Ciphertext homomorphic_sine(Ciphertext &x,
     evaluator.add(res, power4);
     evaluator.add(res, power6);
     evaluator.add(res, power8);
-
-    return res;
 }
 
 #endif
--- tests/test_decode.cpp
+++ tests/test_decode.cpp
@@ -1,13 +1,52 @@
 #include "seal/seal.h"
 #include "fhe_decode.h"
+#include "fhe_image.h"
 
 using namespace seal;
 
-#define APPROX(a, b, epsilon) ((a - b) < epsilon && (b - a) < epsilon)
+#define APPROX(a, b, epsilon) ((a - b) <= epsilon && (b - a) <= epsilon)
 
 
 int main(int argc, const char** argv) {
-    assert(APPROX(1, 2, 1));
-    assert(APPROX(0.9, 1, 0.05));
+
+    int n_number_coeffs = N_NUMBER_COEFFS;
+    int n_fractional_coeffs = N_FRACTIONAL_COEFFS;
+    int n_poly_base = POLY_BASE;
+    int plain_modulus = 1 << 10;
+    int coeff_modulus = COEFF_MODULUS;
+
+    EncryptionParameters params;
+    char poly_mod[16];
+    snprintf(poly_mod, 16, "1x^%i + 1", coeff_modulus);
+    params.set_poly_modulus(poly_mod);
+    params.set_coeff_modulus(coeff_modulus_128(coeff_modulus));
+    params.set_plain_modulus(plain_modulus);
+    SEALContext context(params);
+    print_parameters(context);
+
+    KeyGenerator keygen(context);
+    auto public_key = keygen.public_key();
+    auto secret_key = keygen.secret_key();
+    
+    // Encrytor and decryptor setup
+    Encryptor encryptor(context, public_key);
+    Evaluator evaluator(context);
+    Decryptor decryptor(context, secret_key);
+
+    FractionalEncoder encoder(context.plain_modulus(), context.poly_modulus(), n_number_coeffs, n_fractional_coeffs, n_poly_base);
+
+
+    std::vector<double> test_numbers = {1, 2, 3, 4, 5, 6, 7};
+    for (int i = 0; i < test_numbers.size(); i++) {
+        Ciphertext c, res;
+        Plaintext p;
+        encryptor.encrypt(encoder.encode(test_numbers[i]),c);
+        homomorphic_sine(c, res, evaluator, encoder, encryptor);
+        std::cout << "Noise Budget Left: " << decryptor.invariant_noise_budget(res) << std::endl;
+        decryptor.decrypt(res, p);
+        std::cout << "Res: " << encoder.decode(p) << " Actual Sin: " << sin(test_numbers[i]) << std::endl;
+    }
+
+    
     return 0;
 }
