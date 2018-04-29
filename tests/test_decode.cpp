b6b69409dc6eae2bb671adf3eef47bdfdb8a68b4
WFU <wfu@college.harvard.edu>
2018-04-29 13:16:22 -0400

    [decode] fixed some broken function signatures..

--- tests/test_decode.cpp
+++ tests/test_decode.cpp
@@ -1,9 +1,13 @@
 #include "seal/seal.h"
-#include "fhe_image.h"
+#include "fhe_decode.h"
 
 using namespace seal;
 
-int main(int argc, const char** argv) {
+#define APPROX(a, b, epsilon) ((a - b) < epsilon && (b - a) < epsilon)
 
+
+int main(int argc, const char** argv) {
+    assert(APPROX(1, 2, 1));
+    assert(APPROX(0.9, 1, 0.05));
     return 0;
 }
