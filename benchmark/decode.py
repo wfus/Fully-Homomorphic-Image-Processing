11ab4baf11ea10e4df2b7f0f67fcf368ce9dabe9
Raymond Lin <raylin1000@gmail.com>
2018-04-29 19:30:09 -0400

    Change calling mechanism

--- benchmark/decode.py
+++ benchmark/decode.py
@@ -1,11 +1,10 @@
-import os
+from subprocess import call
 
 CWD = "../"
 DEGREE = [8, 12, 16, 32, 48, 63]
 DELTA = [0.1, 0.2, 0.3, 0.4, 0.5]
-os.system("../bin/decode_client -s")
+call(['./bin/decode_client', '-s'], cwd=CWD)
 for degree in DEGREE:
     for delta in DELTA:
-        os.system("../bin/decode_server --degree {} --delta {}".format(degree, delta))
-        os.system("../bin/decode_client -r -o {}".format('out_{}_{}.png'.format(degree, delta)))
-                    
+        call(['./bin/decode_server', '--degree', str(degree), '--delta', str(delta)], cwd=CWD)
+        call(['./bin/decode_client', '-s', '-o', 'out_{}_{}.png'.format(degree, delta)], cwd=CWD)                    
